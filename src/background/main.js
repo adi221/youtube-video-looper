eb6a245adf0694bac66df520ec6588c36fe359ff
AdimAnyVision <adi.mizrahi@oosto.com>
2023-05-11 23:43:06 +0300

    Change icon theme based on enabled status

--- src/background/main.js
+++ src/background/main.js
@@ -6,3 +6,35 @@ chrome.runtime.onMessage.addListener(function (message) {
     }
   });
 });
+
+// Change the browserAction icon if the extension is disabled
+const activeIconsPaths = {
+  16: '/icons/icon16.png',
+  32: '/icons/icon32.png',
+  64: '/icons/icon64.png',
+  128: '/icons/icon128.png',
+};
+const inactiveIconsPaths = {
+  16: '/icons/icon_disabled16.png',
+  32: '/icons/icon_disabled32.png',
+  64: '/icons/icon_disabled64.png',
+  128: '/icons/icon_disabled128.png',
+};
+
+function changeBrowserIconsIfEnabledStatusChanged(changes) {
+  const {
+    playerStorageData: {
+      newValue: { isEnabled: isEnabledNewValue },
+      oldValue: { isEnabled: isEnabledOldValue } = {},
+    },
+  } = changes;
+  if (isEnabledNewValue === isEnabledOldValue) return;
+  chrome.action.setIcon({
+    path: isEnabledNewValue ? activeIconsPaths : inactiveIconsPaths,
+  });
+}
+
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (area !== 'local' || !changes?.playerStorageData) return;
+  changeBrowserIconsIfEnabledStatusChanged(changes);
+});
